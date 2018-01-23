852d97360b967f9d8eecc21882851b5c50bd2ab9
bigfeats <bigfeats@protonmail.com>
2018-01-23 11:52:31 -0500

    Handle upper case and non-letter characters

--- src/algorithms/shift.rs
+++ src/algorithms/shift.rs
@@ -9,7 +9,7 @@ pub fn encrypt(message: &String, key: &String) -> String {
         Err(_message) => return format!("Invalid key for shift cipher: {}", key)
     };
 
-    shift_string(message, key)
+    shift_string(&message.to_lowercase(), key)
 }
 
 pub fn decrypt(message: &String, key: &String) -> String {
@@ -20,16 +20,19 @@ pub fn decrypt(message: &String, key: &String) -> String {
         Err(_message) => return format!("Invalid key for shift cipher: {}", key)
     };
 
-    shift_string(message, -key)
+    shift_string(&message.to_lowercase(), -key)
 }
 
 fn shift_string(message: &String, offset: i32) -> String {
     let mut ciphertext = String::new();
 
     for letter in message.chars() {
-        let shifted_letter = shift_letter(letter, offset);
-
-        ciphertext = ciphertext.add(shifted_letter.to_string().as_str());
+        if letter.is_alphabetic() {
+            let shifted_letter = shift_letter(letter, offset);
+            ciphertext = ciphertext.add(shifted_letter.to_string().as_str());
+        } else {
+            ciphertext = ciphertext.add(letter.to_string().as_str());
+        }
     }
 
     ciphertext
